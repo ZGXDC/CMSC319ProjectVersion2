a9993682e2dab44d5ad111ff2d0ba804c16f9561
Megan Seaman <meganseaman@megans-mbp.mynetworksettings.com>
2025-11-24 14:24:22 -0500

    added getToken and getArtistInfo methods

--- MainWorkspace.py
+++ MainWorkspace.py
@@ -1,1 +1,87 @@
-print("Hello World!")
+from dotenv import load_dotenv
+import os
+import base64
+import requests
+
+#METHOD getAccessToken
+# makes a post request to Spotify Web API url https://accounts.spotify.com/api/token
+# using clientID and clientSecret credentials
+# Uses Client Credentials Flow
+# - accepts no parameters, returns an access token
+
+def getAccessToken():
+    clientID = os.getenv("clientID")
+    clientSecret = os.getenv("clientSecret")
+    
+    authorizationString = f"{clientID}:{clientSecret}"
+    #Client Credentials Floq requires that the string is Base64 Encoded 
+    b64AuthString = base64.b64encode(authorizationString.encode()).decode()
+    
+    url = "https://accounts.spotify.com/api/token"
+    
+    headers = {
+        "Authorization": f"Basic {b64AuthString}",
+    }
+   
+    data = {
+        "grant_type": "client_credentials"
+    }
+    
+    #Makes HHTP Post request to URL, returns response object
+    try:
+        response = requests.post(url, headers=headers, data=data)
+    except Exception as e:
+        print("ERROR: ", e)
+        print(response.text)
+        
+    if response.status_code !=200:
+        raise Exception(response.status_code)
+   
+    #Converts JSON Response to Python Dictionary
+    token = response.json()
+    return token["access_token"]
+
+#METHOD getArtistIDAndGenre
+# finds an artist's ID and music genres from the Spotify Web API search URL
+# takes a name parameter and makes a get request to the API
+# returns a dictionary object with the artists ID, name, and genres
+def getArtistIDAndGenre(name):
+    if name == "" or name.strip()=="":
+        return None
+    
+    accesstoken = getAccessToken()
+    url = "https://api.spotify.com/v1/search"
+    headers = {"Authorization": f"Bearer {accesstoken}"}
+    params = {
+        "q": name,
+        "type" : "artist",
+        "limit": 1
+    }
+
+    #Makes a GET request to search url and returns a Response object with HTTP 
+    try:
+        response = requests.get(url, headers=headers, params=params)
+    except Exception as e:
+        print("ERROR: ", e)
+    
+    if response.status_code != 200:
+        raise Exception(response.text)
+   
+   
+    #puts json into python dictionary
+    jsonData = response.json()
+    
+    items = jsonData.get("artists", {}).get("items", [])
+    
+    #If the input artist has no matches, none is returned
+    if not items:
+        return None
+    
+    artist = items[0]
+    artistInfo = {
+        "id": artist["id"],
+        "name": artist["name"],
+        "genres": artist["genres"]
+    }
+    return artistInfo
+
--- testing.py
+++ testing.py
@@ -1,7 +1,8 @@
 #This is a python script providing unit tests and integration tests for the playlist generator code
-
+import requests
 from dotenv import load_dotenv
 import os
+from MainWorkspace import getAccessToken, getArtistIDAndGenre
 
 load_dotenv()
 
@@ -18,8 +19,55 @@ def testEnv():
     if clientID is not None and clientSecret is not None:
         print("ClientID: ", clientID)
         print("ClientSecret: ", clientSecret)
- 
+        print("testEnv tests passed")
+        
+def testGetAccessToken():
+    token = getAccessToken()
+    failure = False
+    if token is None:
+       print("ERROR: token is none")
+       failure = True
+    if len(token) < 10:
+        print("ERROR: token is small")
+        failure = True
+    if token is not None:
+        print("Access Token: ", token)
+        
+    if failure == False:
+        print("Tests passed for getAccessToken()")
+
+def testGetArtistIDAndGenre():
+    artist = getArtistIDAndGenre("Nirvana")
+    failure = False
     
-print("UNIT TESTS")
+    if artist is None:
+        print("ERROR: artist is none")
+    if "id" in artist is None:
+        print("ERROR: id is none")
+    if "name" in artist is None:
+        print("ERROR: name is none")
+    if "genres" in artist is None:
+        print("ERROR: genres is none")
+        
+    #EXTREME CASES
+    print(getArtistIDAndGenre("Nirvanaaaaa"))
+    print(getArtistIDAndGenre("irvana"))
+    #Should be None
+    print(getArtistIDAndGenre("doesNotExist"))
+    print(getArtistIDAndGenre("   "))
+    print(getArtistIDAndGenre("\n"))
+    print(getArtistIDAndGenre(""))
+    print(getArtistIDAndGenre("??"))
+  
+
+#PRINTING UNIT TESTS
+print("UNIT TESTS\n")
+
 print('LOADING ENV VARIABLES')
 testEnv()
+
+print('\nTEST FOR getAccessToken()')
+testGetAccessToken()
+
+print("\nTests for getArtistIdAndGenre")
+testGetArtistIDAndGenre()
